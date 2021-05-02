907af2e58c5ffcf50a9e7e1005f7e18ff1b7e7ba
ewaldmikolaj <mikolajewald@gmail.com>
2021-05-02 16:23:31 +0200

    3 projekt

---
+++ projekt_grafy/graph.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+
+int ns[] = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+
+char *insert_names[] = {""};
+
+int main () {
+  return 0;
+}
